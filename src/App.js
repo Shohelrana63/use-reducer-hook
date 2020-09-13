b2b7883c524f1a9808bf99e9074f2624680365b2
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-13 15:23:23 +0600

    basic reducer hook

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PatientManagement from './components/PatientManagement/PatientManagement';
+import ReducerCount from './components/ReducerCount/ReducerCount';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerCount></ReducerCount>
+      <PatientManagement></PatientManagement>
+    </div>
+  );
+}
+
+export default App;
